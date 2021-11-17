b5eb2dfb5b6584cc1ccf5a25bd1e891f691c65f9
thomaami000 <thomaami000@warren.k12.in.us>
2021-11-17 13:20:16 -0500

    shipping cost

--- L2-If Else Statements/src/ShippingCosts.java
+++ L2-If Else Statements/src/ShippingCosts.java
@@ -1,3 +1,5 @@
+import javax.swing.*;
+
 public class ShippingCosts {
 
     /*
@@ -22,5 +24,39 @@ public class ShippingCosts {
     As always, your program should include a method.
      */
 
+    public class Prom {
+
+        /* In order to purchase a ticket to go to prom,
+        you must be a Junior or Senior, have no disciplinary
+        records and have $40 to pay for it.
+         */
+        public static void main(String[] args) {
+
+            int gradeLevel = Integer.parseInt(JOptionPane.showInputDialog("What is your grade level? (9,10,11,12)"));
+            int discipline = Integer.parseInt(JOptionPane.showInputDialog("How many suspensions have you had? "));
+            int money = Integer.parseInt(JOptionPane.showInputDialog("How much money, rounded down to the nearest $5, do you have? "));
+
+            if (gradeLevel >= 11) {
+                if (discipline >= 1) {
+                    JOptionPane.showMessageDialog(null, "You can't get the ticket. ");
+                }else {
+
+                    if(money >= 40){
+                        JOptionPane.showMessageDialog(null, "You can get a ticket! ");
+                    }else{
+                        JOptionPane.showMessageDialog(null, "You can't get the ticket. ");
+                    }
+                }
+            }else {
+                JOptionPane.showMessageDialog(null, "You can't get the ticket. ");
+            }
+        }
+
+
+
+
+
+
+    }
 
 }
